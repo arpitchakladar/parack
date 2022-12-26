23bde388e1f53785c1a329468778c595d89a9283
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2022-12-26 14:32:40 +0530

    Added the ability to combine combinations.

--- src/algorithms/targeted_guess.rs
+++ src/algorithms/targeted_guess.rs
@@ -4,12 +4,16 @@ use std::collections::BTreeMap;
 
 use serde_yaml;
 
-struct NamePermutations<'a> {
+trait Combination: Iterator<Item = String> {
+	fn reset(&mut self);
+}
+
+struct NameCombinations<'a> {
 	name: &'a str,
 	count: usize,
 }
 
-impl<'a> NamePermutations<'a> {
+impl<'a> NameCombinations<'a> {
 	fn new(name: &'a str) -> Self {
 		Self {
 			name,
@@ -18,7 +22,7 @@ impl<'a> NamePermutations<'a> {
 	}
 }
 
-impl Iterator for NamePermutations<'_> {
+impl Iterator for NameCombinations<'_> {
 	type Item = String;
 
 	fn next(&mut self) -> Option<Self::Item> {
@@ -33,13 +37,19 @@ impl Iterator for NamePermutations<'_> {
 	}
 }
 
-struct NumberPermutations<'a> {
+impl Combination for NameCombinations<'_> {
+	fn reset(&mut self) {
+		self.count = 0;
+	}
+}
+
+struct NumberCombinations<'a> {
 	number: &'a str,
 	start: usize,
 	end: usize
 }
 
-impl<'a> NumberPermutations<'a> {
+impl<'a> NumberCombinations<'a> {
 	fn new(number: &'a str) -> Self {
 		Self {
 			number,
@@ -49,7 +59,7 @@ impl<'a> NumberPermutations<'a> {
 	}
 }
 
-impl Iterator for NumberPermutations<'_> {
+impl Iterator for NumberCombinations<'_> {
 	type Item = String;
 
 	fn next(&mut self) -> Option<Self::Item> {
@@ -68,6 +78,61 @@ impl Iterator for NumberPermutations<'_> {
 	}
 }
 
+impl Combination for NumberCombinations<'_> {
+	fn reset(&mut self) {
+		self.start = 0;
+		self.end = self.number.len();
+	}
+}
+
+struct CombineCombinations<T1: Combination, T2: Combination> {
+	permutations1: T1,
+	permutations2: T2,
+	current_permutation: String
+}
+
+impl<T1: Combination, T2: Combination> CombineCombinations<T1, T2> {
+	fn new(mut permutations1: T1, permutations2: T2) -> Self {
+		Self {
+			current_permutation: permutations1.next().unwrap(),
+			permutations1,
+			permutations2
+		}
+	}
+
+	fn add<T3: Combination>(self, permutation: T3) -> CombineCombinations<Self, T3> {
+		CombineCombinations::new(self, permutation)
+	}
+}
+
+impl<T1: Combination, T2: Combination> Iterator for CombineCombinations<T1, T2> {
+	type Item = String;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match self.permutations2.next() {
+			Some(ref permutation) => Some(self.current_permutation.clone() + permutation),
+			None => {
+				match self.permutations1.next() {
+					Some(next_permutation) => {
+						self.permutations2.reset();
+						self.current_permutation = next_permutation;
+						self.next()
+					},
+					None => None
+				}
+			}
+		}
+	}
+}
+
+impl<T1: Combination, T2: Combination> Combination for CombineCombinations<T1, T2> {
+	fn reset(&mut self) {
+		self.permutations1.reset();
+		self.permutations2.reset();
+		self.current_permutation = self.permutations1.next().unwrap();
+	}
+}
+
 pub fn targeted_guess<F>(hash: F, target_information_file: &str, password: &str) -> Option<String>
 	where
 		F:  Fn(&str) -> String
@@ -77,10 +142,12 @@ pub fn targeted_guess<F>(hash: F, target_information_file: &str, password: &str)
 	drop(file);
 	if let Some(names) = target_information.get("names") {
 		if let Some(numbers) = target_information.get("numbers") {
-			for name in NamePermutations::new(&names[0]) {
-				for number in NumberPermutations::new(&numbers[0]) {
-					println!("{}{}", name, number);
-				}
+			let password_combinations = CombineCombinations::new(
+				NameCombinations::new(&names[0]),
+				NumberCombinations::new(&numbers[0])
+			).add(NameCombinations::new(&names[1]));
+			for passwords in password_combinations {
+				println!("{}", passwords);
 			}
 		}
 	}
